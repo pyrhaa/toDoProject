ba90f17f53f9443c00813cfb65f50f64bc794189
pyrhaa <sboubtane@protonmail.com>
2021-04-20 15:50:02 +0200

    database for add & remove ok

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 > Practice in vanilla JS
 
 Source: https://github.com/iamshaunjp/modern-javascript/tree/lesson-77
+
+I put some changes like a database in a json file and others functions with fetch().
+
+To start the database server, for exemple, put this in the terminal : yarn json-server --watch ./data/db.json --port 3001
--- app.js
+++ app.js
@@ -1,11 +1,41 @@
 const addForm = document.querySelector('.add');
 const list = document.querySelector('.todos');
+const url = 'http://localhost:3001/todos/';
 
+//// Add new object TODO in database /////
+
+//id unique per new todo
+const addToDatabase = (content) => {
+  fetch(url, {
+    method: 'POST',
+    body: JSON.stringify({ id: Math.random(), content: content }),
+    headers: {
+      'Content-type': 'application/json'
+    }
+  }).then((response) => {
+    if (response.ok) {
+      return response.json();
+    }
+  });
+};
+
+//delete todo from database(json) with his unique random ID
+const deleteFromDatabase = (id) => {
+  fetch(url + id, { method: 'DELETE' }).then((response) => {
+    if (response.ok) {
+      return response.json();
+    }
+  });
+};
+
+///// ADD or DELETE todos on HTML  /////
+
+// add new todos
 const generateTemplate = (todo) => {
   const html = `
   <li class="list-group-item d-flex justify-content-between align-items-center text-light">
-    <span>${todo}</span>
-    <i class="far fa-trash-alt delete"></i>
+    <span>${todo.content}</span>
+    <i id=${todo.id} class="far fa-trash-alt delete"></i>
   </li>
   `;
   list.innerHTML += html;
@@ -16,6 +46,7 @@ addForm.addEventListener('submit', (e) => {
   const todo = addForm.add.value.trim();
   if (todo.length) {
     generateTemplate(todo);
+    addToDatabase(todo);
     addForm.reset();
   }
 });
@@ -24,5 +55,26 @@ addForm.addEventListener('submit', (e) => {
 list.addEventListener('click', (e) => {
   if (e.target.classList.contains('delete')) {
     e.target.parentElement.remove();
+    console.log(e.target.id);
+    deleteFromDatabase(e.target.id);
+  }
+});
+
+//////    Display of DATABASE //////
+
+//collect database, return a promise
+// typeof db: Promise
+const db = fetch(url, { method: 'GET' }).then((response) => {
+  return response.json();
+});
+
+//log result of promise, get each objet of my res in database
+//typeof res: Array[{},{}, ...]
+//typeof todo: Object {id, content}
+db.then((res) => {
+  console.log(res);
+  for (const todo of res) {
+    console.log(todo);
+    generateTemplate(todo);
   }
 });
